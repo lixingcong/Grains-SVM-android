3dfc904d45269e50a7bfe09c7979a173b651493c
lixingcong <lixingcong@live.com>
2016-12-03 20:26:07 +0800

    activity_main.xml: add 3 buttons

--- app/src/main/java/li/grains/MainActivity.java
+++ app/src/main/java/li/grains/MainActivity.java
@@ -7,7 +7,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
 	};
 	private TextView text1=null;
 
+	private Button btn_run_svm;
+	private Button btn_update_features;
+	private Button btn_download_features;
+	private Button btn_change_svm_params;
+	private LinearLayout layout_update_features;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,13 +56,27 @@ public class MainActivity extends AppCompatActivity {
 			Toast.makeText(getApplicationContext(),"Good permissions",Toast.LENGTH_SHORT).show();
 		}
 
+		layout_update_features=(LinearLayout)findViewById(R.id.layout_main_update);
+		btn_download_features=(Button)findViewById(R.id.button_main_update_features);
+		btn_download_features.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				int is_visible=layout_update_features.getVisibility();
+				if(is_visible==View.VISIBLE){
+					layout_update_features.setVisibility(View.GONE);
+				}else{
+					layout_update_features.setVisibility(View.VISIBLE);
+				}
+			}
+		});
+
 		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
 		Mat mr1 = m.row(1);
 		mr1.setTo(new Scalar(1));
 		Mat mc5 = m.col(5);
 		mc5.setTo(new Scalar(5));
 
-		text1=(TextView)findViewById(R.id.text1);
+		text1=(TextView)findViewById(R.id.textview_main_text1);
 		text1.setText(m.dump());
 	}
 
