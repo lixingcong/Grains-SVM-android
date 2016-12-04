02f9149379c7ce145d8c25ed82ab9516dcb9449e
lixingcong <lixingcong@live.com>
2016-12-04 16:04:34 +0800

    RunSVMActivity: function prototype

--- grains-svm/src/main/java/li/grains/RunSVMActivity.java
+++ grains-svm/src/main/java/li/grains/RunSVMActivity.java
@@ -2,7 +2,9 @@ package li.grains;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +18,8 @@ import android.widget.Toast;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import li.grains.ml.My_Features;
@@ -27,8 +31,11 @@ public class RunSVMActivity extends AppCompatActivity {
 	private My_SVM my_svm=null;
 	private boolean is_svm_has_been_intialized;
 	private boolean is_got_train_features;
+	private boolean is_cropped;
 	private List<Double> train_y=null;
 	private List<List<Double>> train_x=null;
+	private TextView textview1=null;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,7 @@ public class RunSVMActivity extends AppCompatActivity {
 		set_my_view();
 		is_svm_has_been_intialized=false;
 		is_got_train_features=false;
+		is_cropped=false;
 	}
 
 	@Override
@@ -44,12 +52,15 @@ public class RunSVMActivity extends AppCompatActivity {
 		if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
 			CropImage.ActivityResult result = CropImage.getActivityResult(data);
 			if (resultCode == RESULT_OK) {
+				save_bitmap_to_local(result.getBitmap(),getString(R.string.img_filename_cropped));
 				((ImageView) findViewById(R.id.imageview_runsvm_inputimg)).setImageURI(result.getUri());
 				Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
 				set_add_img_visible(false);
+				is_cropped=true;
 			} else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
 				Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
 				set_add_img_visible(true);
+				is_cropped=false;
 			}
 		}
 	}
@@ -59,6 +70,8 @@ public class RunSVMActivity extends AppCompatActivity {
 		final Button btn_trained_pic=(Button)findViewById(R.id.button_runsvm_trainedpic);
 		final Button btn_run=(Button)findViewById(R.id.button_runsvm_run);
 		final ImageView imageview_add_logo=(ImageView)findViewById(R.id.imageview_runsvm_addlogo);
+
+		textview1=(TextView)findViewById(R.id.textview_runsvm_text1);
 
 		// set title
 		setTitle("Run SVM");
@@ -89,6 +102,9 @@ public class RunSVMActivity extends AppCompatActivity {
 
 				if(is_svm_has_been_intialized==false)
 					initSVM();
+
+				if(is_cropped)
+					predict_this_picture();
 			}
 		});
 	}
@@ -138,6 +154,9 @@ public class RunSVMActivity extends AppCompatActivity {
 				if(is_got_train_features==false)
 					loadTrainFeatures();
 
+				// train
+				my_svm.train(train_x,train_y);
+
 				// dismiss it
 				progress.dismiss();
 			}
@@ -162,5 +181,36 @@ public class RunSVMActivity extends AppCompatActivity {
 		train_y=features_train.get_features_y();
 		train_x=features_train.get_features_x();
 		is_got_train_features=true;
+	}
+
+	private void save_bitmap_to_local(Bitmap bitmap,String filename){
+		FileOutputStream out = null;
+		try {
+			// Locate storage location
+			String filepath = Environment.getExternalStorageDirectory().getPath();
+			out = new FileOutputStream(filepath+"/"+filename);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void predict_this_picture(){
+		String filename = Environment.getExternalStorageDirectory().getPath();
+		filename+=("/"+getString(R.string.img_filename_cropped));
+		Log.v("SVM",filename);
+		My_Features features_test=new My_Features(filename);
+		List<List<Double>> test_x=features_test.get_features_x();
+		List<Double> predict_y= my_svm.predict(test_x);
+		String result=features_train.get_chinese_from_category(new Double(predict_y.get(0)).intValue());
+		textview1.setText(result);
 	}
 }
